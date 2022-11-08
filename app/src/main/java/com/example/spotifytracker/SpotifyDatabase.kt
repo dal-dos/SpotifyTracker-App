07db3e522403c1bbd38875a25405417e1f72ca1f
Dalveer Dosanjh <dsd6@sfu.ca>
2022-11-07 16:47:23 -0800

    Added Initial Database Files

--- app/src/main/java/com/example/spotifytracker/SpotifyDatabase.kt
+++ app/src/main/java/com/example/spotifytracker/SpotifyDatabase.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.dalveer_dosanjh_myruns4.SpotifyDataDao
-import com.example.dalveer_dosanjh_myruns4.SpotifyDataEntity
 
 @Database(entities = [SpotifyDataEntity::class], version = 1)
 abstract class SpotifyDatabase: RoomDatabase() {
