555d370a7d959739ea17e1d1f0db90bcf8d8d41d
Daven <dca120@sfu.ca>
2022-11-09 16:24:56 -0800

    Testing

--- app/src/main/java/com/example/spotifytracker/LoginActivity.kt
+++ app/src/main/java/com/example/spotifytracker/LoginActivity.kt
@@ -12,6 +12,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         title=""
+        // Test comment
     }
 
     fun onClickLogin(view: View) {
